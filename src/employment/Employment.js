9118395dcb690f7a67ab49afe7e4a79a77d95218
Rishee Jayasimha <jayasimha.3@osu.edu>
2023-09-10 23:05:56 -0400

    adding components

--- src/employment/Employment.js
+++ src/employment/Employment.js
@@ -4,12 +4,16 @@ import me from '../static/img/homepage/jayasimharishee.jpg';
 import { rishee } from '../common/Constants.js';
 import { Navbar } from '../common/Navbar.js';
 
+import BracketBug from '../components/BracketBug.js';
+import { ChampionsBowlBug } from '../components/BracketBackground';
+
 function EmploymentBody() {
     return (
       <>
       <Navbar></Navbar>
         <div class="container-xl bg-dark" flex>  
           <p>This is the Employment Page</p>
+          <ChampionsBowlBug></ChampionsBowlBug>
         </div>
       </>
     )
