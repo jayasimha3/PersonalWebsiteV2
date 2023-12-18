e0f4409426a1b845dc3c3b4f7930e367405e4ced
Rishee Jayasimha <jayasimha3.osu.edu>
2023-12-18 01:02:48 -0500

    added footer

--- src/extracurricular/Extracurricular.jsx
+++ src/extracurricular/Extracurricular.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import me from '../static/img/homepage/jayasimharishee.jpg';
 import { rishee } from '../common/Constants.js';
-import { Navbar } from '../common/Navbar.js';
+import { Navbar, Footer } from '../common/Navbar.js';
 
 function ExtracurricularBody() {
     return (
@@ -148,6 +148,7 @@ function ExtracurricularBody() {
             </div>
         </div>
         </div>
+    <Footer></Footer>
     </>
     )
   }
