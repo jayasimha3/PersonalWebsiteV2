0e0c7016af38f0900421e73a0257a168dc73a9b6
Rishee Jayasimha <jayasimha3.osu.edu>
2024-02-11 00:34:21 -0500

    fixed build error, committing changes to App.test.tsx forgot to commit last time

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name as heading', () => {
   render(<App />);
   const linkElement = screen.getByText(/Rishee Jayasimha/i);
   expect(linkElement).toBeInTheDocument();
