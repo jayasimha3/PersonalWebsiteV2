49d9fbf3fc0a98cb049e122345567b3fcfd98631
Rishee Jayasimha <jayasimha3.osu.edu>
2024-02-10 23:36:01 -0500

    fixed all errors from js to ts conversion, added a github action for validation on PR

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -4,67 +4,67 @@ import { Link } from 'react-router-dom'
 
 const navbarMode = 'navbar navbar-expand-lg bg-dark'; // dark mode, need to switch bg- to light for light mode
 
-function Navbar() {
+export function Navbar() {
     return (
-      <nav class={ navbarMode }>
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className={ navbarMode }>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class='nav-link' href='/'>Home</a>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className='nav-link' href='/'>Home</a>
               </li>
-              <li class="nav-item">
-                <a class='nav-link' href='/employment'>Work Experience</a>
+              <li className="nav-item">
+                <a className='nav-link' href='/employment'>Work Experience</a>
               </li>
-              <li class="nav-item">
-                <a class='nav-link' href='/projects'>Projects</a>
+              <li className="nav-item">
+                <a className='nav-link' href='/projects'>Projects</a>
               </li>
-              <li class="nav-item">
-                <a class='nav-link' href='/extracurricular'>Extracurricular</a>
+              <li className="nav-item">
+                <a className='nav-link' href='/extracurricular'>Extracurricular</a>
               </li>
             </ul>
           </div>
         </div>
-        <a tabindex="0" class="btn btn-primary" role="button" data-toggle="popover" data-trigger="hover focus" data-placement="bottom" data-html="true" data-content="<em>Email</em>: jayasimha.3@osu.edu">
+        <a tabIndex={0} className="btn btn-primary" role="button" data-toggle="popover" data-trigger="hover focus" data-placement="bottom" data-html="true" data-content="<em>Email</em>: jayasimha.3@osu.edu">
             Contact
         </a>
       </nav>
-    )
+    );
   }
 
-function Footer() {
+export function Footer() {
   return (
-    <footer class="navbar bg-dark navbar-expand-lg sticky-bottom">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <footer className="navbar bg-dark navbar-expand-lg sticky-bottom">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <a class="navbar-brand" href="https://github.com/jayasimha3">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <a className="navbar-brand" href="https://github.com/jayasimha3">
             <img src="../logo.svg" alt="" width="32" height="32" title="Github"></img>
           </a>
-          <a class="navbar-brand" href="https://www.linkedin.com/in/jayasimha3/">
+          <a className="navbar-brand" href="https://www.linkedin.com/in/jayasimha3/">
             <img src="../logo.svg" alt="" width="32" height="32" title="LinkedIn"></img>
           </a>
-          <a class="navbar-brand" href="https://osu.joinhandshake.com/users/11578467">
+          <a className="navbar-brand" href="https://osu.joinhandshake.com/users/11578467">
             <img src="../logo.svg" alt="" width="32" height="32" title="Handshake"></img>
           </a>
-          <a class="navbar-brand" href="https://my.indeed.com/p/risheej-w1nbb5b">
+          <a className="navbar-brand" href="https://my.indeed.com/p/risheej-w1nbb5b">
             <img src="../logo.svg" alt="" width="64" height="32" title="Indeed"></img>
           </a>
         </ul>
-        <span class="navbar-text">
+        <span className="navbar-text">
           Rishee Jayasimha | 
-          <a class="text-monospace text-muted" href="https://github.com/jayasimha3/jayasimha3.github.io/tree/master/assets/CHANGELOG.md">Version react2.0.1</a>
+          <a className="text-monospace text-muted" href="https://github.com/jayasimha3/jayasimha3.github.io/tree/master/assets/CHANGELOG.md">Version react2.0.1</a>
         </span>
       </div>
     </footer>
-  )
+  );
 }
 
-export { Navbar, Footer };
+//export { Navbar, Footer };
