2b098dc67dceb2f0bd9c65910e9e9d83631237ea
Rishee Jayasimha <jayasimha3.osu.edu>
2024-03-02 22:13:08 -0500

    updates

--- src/homepage/Homepage.tsx
+++ src/homepage/Homepage.tsx
@@ -1,47 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../common/styles.css';
 import { rishee } from '../common/Constants';
+import headshot from '../public/images/me/jayasimharishee.jpg';
+
+const homepage_card_img_style: React.CSSProperties = {
+  maxWidth: "40%"
+};
 
 function HomepageImage() {
     return (
-        <img src='../static/img/homepage/jayasimharishee.jpg' className="img-fluid" alt={ rishee.name }></img>
+        <img src={headshot} className="img-fluid" alt={rishee.name}></img>
     );
 }
 
 function HomepageBody() {
     return (
-      <div className="container-xl bg-{ mode } flex">
-          <main role="main" className="inner cover">
-            <div className="card mb-3 main-card border-0" >
-              <div className="d-flex flex-wrap no-gutters">
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h1 className="cover-heading">{ rishee.name }</h1>
-                    <p className="lead">
-                      I am Rishee Jayasimha, a B.S. Computer Science and Engineering alumnus of The Ohio State University in Columbus, OH, and currently working as a software developer at General Motors.
-                      I have completed many projects including using machine learning to rank college football teams, creating an AI-assisted kitchen assistant,
-                      and creating a database for a library. I am a motivated person who enjoys being challenged; I have programmed in SQL, Java, C, and Python, among other langauges.                
-                    </p>
-                    <p className="home text-muted">
-                      Some extracurricular activities I enjoy are playing video games, playing board games, and playing my trumpet. I have auditioned for The Ohio State University Marching Band 
-                      (TBDBITL) and participated in Athletic Band and concert bands at Ohio State.
-                    </p>
-                    <p className="home text-muted">
-                      I am not actively seeking employment opportunities, but will consider any opportunities I might recieve.               
-                    </p>
-                    <p className="lead">
-                      <a href="/about" className="btn btn-lg btn-secondary">Learn more</a>
-                    </p>
-                  </div>
-                </div>
-                <div className="col-md-4 flex">
-                  <HomepageImage></HomepageImage>
-                </div>
-              </div>
-            </div>
-          </main>
+      <div className="card mb-3 flex" style={homepage_card_img_style}>
+  <div className="row g-0">
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">{rishee.name}</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
       </div>
+    </div>
+    <div className="col-md-4">
+      <HomepageImage></HomepageImage>
+    </div>
+  </div>
+</div>
     )
   }
 
-export { HomepageImage, HomepageBody };
+export { HomepageBody };
