ef13639c2e4955f1c1ea6e0def517bee788515e4
Rishee Jayasimha <jayasimha.3@osu.edu>
2024-05-02 21:30:11 -0400

    added about page

---
+++ src/about/About.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../common/styles.css';
+import { Navbar, Footer } from '../common/Navbar';
+import { styles } from '../common/Constants';
+
+function AboutBody() {
+    return (
+        <></>
+    )
+};
+
+export { AboutBody };
