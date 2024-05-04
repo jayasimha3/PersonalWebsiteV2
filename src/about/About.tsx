5a17ba4ddf22f50d56fcf8982b92e8cc18eaa623
Rishee Jayasimha <jayasimha.3@osu.edu>
2024-05-04 00:06:01 -0400

    about page starter

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -5,7 +5,16 @@ import { styles } from '../common/Constants';
 
 function AboutBody() {
     return (
-        <></>
+        <>
+        <Navbar></Navbar>
+        <div className="container-md justify-content-evenly">
+            <h2 className="display-2 text-light">About Me</h2>
+        </div>
+        <br></br>
+        <div className="container-md justify-content-evenly">
+        </div>
+        <Footer></Footer>
+        </>
     )
 };
 
