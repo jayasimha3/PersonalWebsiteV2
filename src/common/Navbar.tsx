9049ab41f162f802f1c8f4b5d8c3c13dea77e22f
Rishee Jayasimha <jayasimha.3@osu.edu>
2024-10-02 20:06:46 -0400

    footer position bottom and stays at the bottom

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './Navbar.css';
 import React from 'react'
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import linkedin from '../static/images/employment/linkedin-blue.png'
 import github from '../static/images/employment/github-white.svg'
 
 const navbarMode = 'navbar navbar-expand-lg bg-dark'; // dark mode, need to switch bg- to light for light mode
-const footerMode = 'navbar navbar-expand-lg bg-dark fixed-bottom'; // dark mode, need to switch bg- to light for light mode
+const footerMode = 'navbar navbar-expand bg-dark sticky-bottom'; // dark mode, need to switch bg- to light for light mode
 
 export function Navbar() {
     return (
-      <nav className={ navbarMode }>
+      <nav className={ navbarMode } data-bs-theme="dark">
         <div className="container-fluid">
           {/* <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,11 +43,8 @@ export function Navbar() {
 
 export function Footer() {
   return (
-    <nav className={footerMode}>
-      {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button> */}
-
+    <nav className={ footerMode } data-bs-theme="dark">
+      {/* <div className="container-fluid flex-column">  */}
       <div className="container-fluid">
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
@@ -58,7 +57,7 @@ export function Footer() {
         </ul>
       </div>
       <span className="navbar-text text-end">
-            Rishee Jayasimha | <a className="font-monospace text-muted" href="https://github.com/jayasimha3/PersonalWebsiteV2/blob/main/CHANGELOG.md">Version react2.0.7</a>
+            Rishee Jayasimha | <a className="font-monospace text-muted" href="https://github.com/jayasimha3/PersonalWebsiteV2/blob/main/CHANGELOG.md">Version 2.1-rc1</a>
         </span>
       </div>
     </nav>
